c32c4e938bd831713a094d0877a5cc2e8a2ec3cc
HENRY JERRY WANDERA <HENRY-JERRY@users.noreply.github.com>
2020-11-27 10:20:05 +0200

    Add files via upload
    added network graph code

---
+++ src/bigram.R
@@ -0,0 +1,33 @@
+source("main.R")
+
+#relationship among the various words in Mon Ami Gabi reviews here through a network graph
+count_bigrams <- function(dataset) {
+  dataset %>%
+    unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+    separate(bigram, c("word1", "word2"), sep = " ") %>%
+    filter(!word1 %in% stop_words$word,
+           !word2 %in% stop_words$word) %>%
+    count(word1, word2, sort = TRUE)
+}
+
+visualize_bigrams <- function(bigrams) {
+  set.seed(2016)
+  a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+  
+  bigrams %>%
+    graph_from_data_frame() %>%
+    ggraph(layout = "fr") +
+    geom_edge_link(aes(edge_alpha = n), show.legend = FALSE, arrow = a) +
+    geom_node_point(color = "lightblue", size = 5) +
+    geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+    theme_void()
+  
+}
+
+bigramsMonAmiGabi <- reviews %>%
+  filter(business_id == "4JNXUYY8wbaaDmk3BPzlWw") %>%
+  count_bigrams()
+
+bigramsMonAmiGabi %>%
+  filter(n > 50) %>%
+  visualize_bigrams()
